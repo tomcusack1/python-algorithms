45221adc632c2de4cb96fab42f30886ebe93b46b
Tom Cusack <tom@tom-cusack.com>
2016-10-18 14:24:29 +0100

    Started linked list exercise

--- Codewars/baker.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from python_ping import ping
-
-# host = Host you want to ping
-p = ping.Ping(host, timeout=3000, quiet=True, silent=True, ipv6=False)
-stats = p.run(1)  # PingStats
